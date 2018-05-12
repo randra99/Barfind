b058af8197e1be935d99b4518586f05b1c801418
Guta_Andrei <limeware48@gmail.com>
2018-05-12 17:21:16 +0300

    Fix 125 and other

--- app/src/main/java/cafecod/com/barfind/LoginPage.java
+++ app/src/main/java/cafecod/com/barfind/LoginPage.java
@@ -136,7 +136,7 @@ public class LoginPage extends AppCompatActivity {
                                     Toast.makeText(LoginPage.this, "Authentication succesfull.",
                                             Toast.LENGTH_SHORT).show();
                                     FirebaseUser user = mAuth.getCurrentUser();
-                                    user.getPhotoUrl().toString()
+                                    user.getPhotoUrl().toString();
                                     updateUI(user);
                                 } else {
                                     // If sign in fails, display a message to the user.
