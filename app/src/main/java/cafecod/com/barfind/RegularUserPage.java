70bd8dad45234943730dab53210f7b4a889dc388
Guta_Andrei <limeware48@gmail.com>
2018-05-13 12:22:56 +0300

    Nav Bar Aestetic 2.0

--- app/src/main/java/cafecod/com/barfind/RegularUserPage.java
+++ app/src/main/java/cafecod/com/barfind/RegularUserPage.java
@@ -80,13 +80,11 @@ public class RegularUserPage extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_view) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_map) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_options) {
 
         } else if (id == R.id.nav_info) {
 
