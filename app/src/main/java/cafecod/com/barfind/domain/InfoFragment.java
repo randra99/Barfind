07bbf1944680c0fb87f14244029d2999489d435a
Guta_Andrei <limeware48@gmail.com>
2018-05-13 10:59:34 +0300

    Info Page and fragmented nav bar

---
+++ app/src/main/java/cafecod/com/barfind/domain/InfoFragment.java
@@ -0,0 +1,7 @@
+package cafecod.com.barfind.domain;
+
+import android.app.Fragment;
+//FRAGMENT DE ANDROID.SUPPORT.APP PESTE TOT!!!!!!!!!!!!!!!!!
+public class InfoFragment extends android.support.v4.app.Fragment {
+
+}
