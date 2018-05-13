9d2435619fa6e026d58927779fde46e40ea13618
Paulllx <inventionpaul@gmail.com>
2018-05-13 12:44:29 +0300

    Radu mi-a spus sa dau push

--- app/src/main/java/cafecod/com/barfind/SecondBarUserPage.java
+++ app/src/main/java/cafecod/com/barfind/SecondBarUserPage.java
@@ -2,12 +2,33 @@ package cafecod.com.barfind;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SecondBarUserPage extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_bar_user_page);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        ImageButton gol = (ImageButton) findViewById(R.id.gol);
+        ImageButton putini = (ImageButton) findViewById(R.id.putini);
+        ImageButton multi = (ImageButton) findViewById(R.id.multi);
+
+        gol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
+
+
