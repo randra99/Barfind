f8d46eab0a79c1749a6c9168de9d09a07daced34
randra99 <radu.andra1999@gmail.com>
2018-05-13 12:21:04 +0300

    E ok

---
+++ app/src/main/java/cafecod/com/barfind/SecondBarUserPage.java
@@ -0,0 +1,13 @@
+package cafecod.com.barfind;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondBarUserPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_bar_user_page);
+    }
+}
